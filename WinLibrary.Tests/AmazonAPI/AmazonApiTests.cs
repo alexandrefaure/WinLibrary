f9112a6226acbcd9b71096fa7e4f0d27c7685d5e
Alexousky <alx.faure@gmail.com>
2016-06-19 18:57:15 +0200

    Ajout de la classe AmazonAPI et des tests associés

---
+++ WinLibrary.Tests/AmazonAPI/AmazonApiTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using WinLibrary.AmazonAPI;
+
+namespace WinLibrary.Tests.AmazonAPI
+{
+    [TestFixture]
+    public sealed class AmazonApiTests
+    {
+        [Test]
+        public void GetAuthenticationTest()
+        {
+            var authentication = AmazonApi.GetAuthentication();
+            Assert.IsNotNull(authentication);
+        }
+
+        [TestCase("9782100738748")] //SCRUM Le guide de la méthode agile la plus populaire
+        [TestCase("")]
+        public void GetIsbnInformation(string isbn)
+        {
+            var informations = AmazonApi.GetInformation(isbn);
+            if (isbn != string.Empty)
+            {
+                Assert.IsNotNull(informations);
+            }
+            else
+            {
+                Assert.IsNull(informations);
+            }
+        }
+    }
+}
---
+++ WinLibrary/AmazonAPI/AmazonApi.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Nager.AmazonProductAdvertising;
+using Nager.AmazonProductAdvertising.Model;
+
+namespace WinLibrary.AmazonAPI
+{
+    public class AmazonApi
+    {
+        private const string AccesKeyId = "AKIAIP6L4WACU6L6GVOA";
+        private const string SecretKeyId = "Gl3jqN8wfInYqlyZwmtjG6VoTsd0q4w3RoqdMOwb";
+
+        public static AmazonAuthentication GetAuthentication()
+        {
+            var authentication = new AmazonAuthentication
+            {
+                AccessKey = AccesKeyId,
+                SecretKey = SecretKeyId
+            };
+            return authentication;
+        }
+        public static Item GetInformation(string isbn)
+        {
+            var wrapper = new AmazonWrapper(GetAuthentication(), AmazonEndpoint.FR);
+            var itemResponse = wrapper.Lookup(isbn, AmazonResponseGroup.Large);
+            if (itemResponse != null && itemResponse.Items.Item != null)
+            {
+                var result = itemResponse.Items.Item.SingleOrDefault();
+                return result;
+            }
+            return null;
+        }
+    }
+}
