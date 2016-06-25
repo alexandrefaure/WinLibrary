277159efa0d2345acdba10e21ba53b53050ace18
Alexousky <alx.faure@gmail.com>
2016-06-26 00:03:40 +0200

    Protect AWS credentials

--- WinLibrary/AmazonAPI/AmazonApi.cs
+++ WinLibrary/AmazonAPI/AmazonApi.cs
@@ -7,8 +7,8 @@ namespace WinLibrary.AmazonAPI
 {
     public class AmazonApi
     {
-        private const string AccesKeyId = "AKIAIP6L4WACU6L6GVOA";
-        private const string SecretKeyId = "Gl3jqN8wfInYqlyZwmtjG6VoTsd0q4w3RoqdMOwb";
+        private const string AccesKeyId = AmazonApiCredentials.AccesKeyId;
+        private const string SecretKeyId = AmazonApiCredentials.SecretKeyId;
 
         public static Book GetBook(string isbn)
         {
